0919335291fdd78a9252f8a0a05d91aa0e13a6bb
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-02-26 20:37:29 +0600

    Auth sytem is installed

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomePageController;
-
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [HomePageController::class, 'index']);
 Route::get('/about-me', [HomePageController::class, 'aboutMe']);
 Route::get('/contact-me', [HomePageController::class, 'contactMe']);
 Route::get('/blog-details', [HomePageController::class, 'blogDetails']);
+
+Auth::routes([
+    'register' => false,
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
